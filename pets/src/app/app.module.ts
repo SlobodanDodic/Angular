2ea8fbfe05db93ba9fcbb159e890a0bb51dc30b9
SlobodanDodic <slobodandodic@gmail.com>
2024-02-14 11:58:01 +0100

    pets - g components

--- pets/src/app/app.module.ts
+++ pets/src/app/app.module.ts
@@ -3,16 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { SearchFormComponent } from './search-form/search-form.component';
+import { PetListComponent } from './pet-list/pet-list.component';
+import { PetComponent } from './pet/pet.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, NavbarComponent, SearchFormComponent, PetListComponent, PetComponent],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
