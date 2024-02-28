04c273884c92cc2e81e2c2449424457018d8bab3
SlobodanDodic <slobodandodic@gmail.com>
2024-02-28 09:41:38 +0100

    a-restaurant initial commit

---
+++ a-restaurant/restaurant/src/app/app-routing.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './core/home/home.component';
+import { AboutComponent } from './core/about/about.component';
+import { RestaurantsComponent } from './restaurants/restaurants/restaurants.component';
+
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'restaurants', component: RestaurantsComponent },
+  { path: '', redirectTo: '/home', pathMatch: 'prefix' },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
---
+++ a-restaurant/restaurant/src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { LeftbarComponent } from './core/leftbar/leftbar.component';
+import { HomeComponent } from './core/home/home.component';
+import { AboutComponent } from './core/about/about.component';
+import { RestaurantsComponent } from './restaurants/restaurants/restaurants.component';
+import { RestaurantItemComponent } from './restaurants/restaurant-item/restaurant-item.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LeftbarComponent,
+    HomeComponent,
+    AboutComponent,
+    RestaurantsComponent,
+    RestaurantItemComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
